0b02344cee09acf86b8339c6d4fb0584a4d8fc50
Kay Mok <kaymok3@gmail.com>
2018-04-24 20:42:58 -0400

    Add form to add attendee

--- app.rb
+++ app.rb
@@ -8,3 +8,12 @@ get '/' do
   @attendees = Attendee.all
   erb :index
 end
+
+get '/attendee/new' do
+  erb :new
+end
+
+post '/attendee/new' do
+  @new_attendee = Attendee.create(first_name: params[:first_name], last_name: params[:last_name], age: params[:age], likes_beer: params[:likes_beer])
+  redirect "/"
+end
