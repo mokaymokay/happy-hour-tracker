7e5ad02800188d8beabfeb013d8fd0d768e7b032
Kay Mok <kaymok3@gmail.com>
2018-04-24 20:59:54 -0400

    Add form to edit attendee info

--- app.rb
+++ app.rb
@@ -17,3 +17,14 @@ post '/attendee/new' do
   @new_attendee = Attendee.create(first_name: params[:first_name], last_name: params[:last_name], age: params[:age], likes_beer: params[:likes_beer])
   redirect "/"
 end
+
+get '/attendee/:id/edit' do
+  @current_attendee = Attendee.find(params[:id])
+  erb :edit
+end
+
+put '/attendee/:id' do
+  @current_attendee = Attendee.find(params[:id])
+  @current_attendee.update(first_name: params[:first_name], last_name: params[:last_name], age: params[:age], likes_beer: params[:likes_beer])
+  redirect "/"
+end
